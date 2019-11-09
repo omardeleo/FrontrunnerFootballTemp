a6bcb6a3fcf5403ed0ecf5d99d8ea5eb9114a1c1
Omar De Leo <omar.deleo@gmail.com>
2019-11-09 13:48:31 -0500

    Import remaining fixture lists

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import { leagueAPI } from "./leagueApiUtil";
 import LeagueTable from "./LeagueTableContainer";
 import premFixtures from "./fixtures_prem.js";
 import serieAFixtures from "./fixtures_serie_a.js";
+import laLigaFixtures from "./fixturesLaLiga.js";
+import bundesligaFixtures from "./fixturesBundesliga.js";
+import ligue1Fixtures from "./fixturesLigue1.js";
 
+import eredivisieFixtures from "./fixturesEredivisie.js";
 function ListOfTables() {
   return (
     <div>
@@ -19,16 +23,28 @@ function ListOfTables() {
         tierLimit={5}
         fixtures={serieAFixtures}
       />
+      <LeagueTable
+        api={leagueAPI.laLiga}
+        tierLimit={5}
+        fixtures={laLigaFixtures}
+      />
+      <LeagueTable
+        api={leagueAPI.bundesliga}
+        tierLimit={5}
+        fixtures={bundesligaFixtures}
+      />
+      <LeagueTable
+        api={leagueAPI.ligue1}
+        tierLimit={4}
+        fixtures={ligue1Fixtures}
+      />
+      <LeagueTable
+        api={leagueAPI.eredivisie}
+        tierLimit={3}
+        fixtures={eredivisieFixtures}
+      />
     </div>
   );
 }
 
 ReactDOM.render(<ListOfTables />, document.querySelector("#root"));
-
-/*
-      
-      <LeagueTable api={leagueAPI.laLiga} tierLimit={5} />
-      <LeagueTable api={leagueAPI.bundesliga} tierLimit={5} />
-      <LeagueTable api={leagueAPI.ligue1} tierLimit={4} />
-      <LeagueTable api={leagueAPI.eredivisie} tierLimit={3} />
-*/
